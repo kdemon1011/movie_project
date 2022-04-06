cb25b533efc4e4940c627998f191e6a15c9956e5
Ketul Parikh <ketul93@gmail.com>
2022-04-06 23:49:29 +0530

    [Update] fetch_movies copmleted

---
+++ README.md
@@ -0,0 +1,25 @@
+# Prerequisites
+
+- Python3
+
+- Create virtual env
+
+  ```bash
+  pip3 uninstall virtualenv
+  sudo apt-get install python-virtualenv
+  python3 -m venv env
+  ```
+
+- Install dependencies
+
+  ```bash
+  pip install -r requirements.txt
+  ```
+
+# Fetch Movies
+
+- Run the following command to fetch top 250 movies from the IMDB website. The data will be stored into **disha_movies.csv** file
+
+  ```bash
+  python fetch_movies.py
+  ```
---
+++ fetch_movies.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import pandas as pd
+from multiprocessing.dummy import Pool
+from multiprocessing import cpu_count
+
+# Download IMDB's Top 250 data
+url = 'http://www.imdb.com/chart/top' # IMDb Top 250 list link
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser') # Get data from the HTML. Alternative is 'lxml'. I prefer 'html.parser'
+
+# As above page soup will contain on ly title, rating and poster information. We need to make request to each movie page to extract the name of the director(s), and the genre information of the movie.
+
+# Get the title links for all the pages
+movie_links = [a.find("td",{"class":"titleColumn"}).a.attrs.get('href') for a in soup.find("tbody", {"class":"lister-list"}).findAll("tr")]
+
+prefix = "https://www.imdb.com%s" # href in the above result contains only postfix component. We need to add prefix to access full url
+
+movie_links = [prefix % link for link in movie_links]
+
+# Store required info i.e., diractors' name and genres using followinf function. SCrping will be done using multiprocessing
+
+def scrape_data(movie_page_url):
+    print(movie_page_url)
+    response = requests.get(movie_page_url)
+    page_soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Extraactin json data which contains all info
+    json_data = json.loads(page_soup.find_all("script",type="application/ld+json")[0].text)
+    
+    title = json_data.get("name",None) # Getting title, if "name" key not found alloating default value None
+    genre_list = json_data.get("genre",[]) # Getting genere_list, if "genere" key not found alloating default value empty list
+    director = json_data.get("director",[{"name":None}])[0].get("name") # Getting director info, if "director" key not found alloating an array with default name to None
+    
+    movie_dict = {
+        "title":title,
+        "genres": genre_list,
+        "director": director
+    }
+
+    return movie_dict
+
+pool = Pool(cpu_count() * 2) # Creating pool based on cores available on machine
+movie_list = pool.map(scrape_data,movie_links)
+# print(movie_list)
+
+df_columns = [ 'title','genres','director']
+df = pd.DataFrame(movie_list, columns=df_columns)
+df.to_csv("disha_movies.csv",index=False) # Removing index
